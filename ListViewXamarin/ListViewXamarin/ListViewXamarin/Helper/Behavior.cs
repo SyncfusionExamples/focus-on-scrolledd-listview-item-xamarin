ff73d1d2e7820eec9ede488e463458fdfff9294e
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-08-12 15:31:36 +0530

    XAMARIN-20826 - Sample and screenshot added.

---
+++ ListViewXamarin/ListViewXamarin/ListViewXamarin/Helper/Behavior.cs
@@ -0,0 +1,66 @@
+﻿using Syncfusion.DataSource.Extensions;
+using Syncfusion.GridCommon.ScrollAxis;
+using Syncfusion.ListView.XForms;
+using Syncfusion.ListView.XForms.Control.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ListViewXamarin
+{
+    public class Behavior : Behavior<ContentPage>
+    {
+        #region Fields
+
+        SfListView ListView;
+        Button Button;
+        VisualContainer Container;
+        #endregion
+
+        #region Overrides
+        protected override void OnAttachedTo(ContentPage bindable)
+        {
+            ListView = bindable.FindByName<SfListView>("listView");
+            Button = bindable.FindByName<Button>("scrollButton");
+            Container = ListView.GetVisualContainer();
+            Button.Clicked += Button_Clicked;
+
+            base.OnAttachedTo(bindable);
+        }
+
+        protected override void OnDetachingFrom(ContentPage bindable)
+        {
+            Button.Clicked -= Button_Clicked;
+            Button = null;
+            ListView = null;
+            Container = null;
+            base.OnDetachingFrom(bindable);
+        }
+        #endregion
+
+        #region Event
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            var index = ListView.DataSource.DisplayItems.Count() - 1;
+            var item = ListView.DataSource.DisplayItems[index];
+            ListView.LayoutManager.ScrollToRowIndex(index, Syncfusion.ListView.XForms.ScrollToPosition.End, true);
+
+            await Task.Delay(1000);
+            for (int i = 0; i < Container.Children.Count; i++)
+            {
+                if (Container.Children[i].BindingContext == item)
+                {
+                    var entry = (Container.Children[i] as ListViewItem).Content as Entry;
+                    entry.Focus();
+                }
+            }
+        }
+        #endregion
+    }
+}
---
+++ ListViewXamarin/ListViewXamarin/ListViewXamarin/ViewModel/ContactsViewModel.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ListViewXamarin
+{
+    public class ContactsViewModel
+    {
+        #region Properties
+
+        public ObservableCollection<Contacts> ContactsInfo { get; set; }
+        #endregion
+
+        #region Constructor
+
+        public ContactsViewModel()
+        {
+            ContactsInfo = new ObservableCollection<Contacts>();
+            GenerateInfo();
+        }
+
+        public void GenerateInfo()
+        {
+            Random r = new Random();
+            for (int i = 0; i < 30; i++)
+            {
+                var contact = new Contacts();
+                contact.ContactName = CustomerNames[i];
+                ContactsInfo.Add(contact);
+            }
+        }
+
+        #endregion
+
+        #region Fields
+
+        string[] CustomerNames = new string[] {
+            "Kyle",
+            "Gina",
+            "Irene",
+            "Katie",
+            "Michael",
+            "Oscar",
+            "Ralph",
+            "Torrey",
+            "William",
+            "Bill",
+            "Daniel",
+            "Frank",
+            "Brenda",
+            "Danielle",
+            "Fiona",
+            "Howard",
+            "Jack",
+            "Larry",
+            "Holly",
+            "Jennifer",
+            "Liz",
+            "Pete",
+            "Steve",
+            "Vince",
+            "Zeke",
+            "Aiden",
+            "Jackson"    ,
+            "Mason  "    ,
+            "Liam   "    ,
+            "Jacob  "    ,
+            "Jayden "    ,
+            "Ethan  "    ,
+            "Noah   "    ,
+            "Lucas  "    ,
+            "Logan  "    ,
+            "Caleb  "    ,
+            "Caden  "    ,
+            "Jack   "    ,
+            "Ryan   "    ,
+            "Connor "    ,
+            "Michael"    ,
+            "Elijah "    ,
+            "Brayden"    ,
+            "Benjamin"   ,
+            "Nicholas"   ,
+            "Alexander"  ,
+            "William"    ,
+            "Matthew"    ,
+            "James  "    ,
+            "Landon "    ,
+            "Nathan "    ,
+            "Dylan  "    ,
+            "Evan   "    ,
+            "Luke   "    ,
+            "Andrew "    ,
+            "Gabriel"    ,
+            "Gavin  "    ,
+            "Joshua "    ,
+            "Owen   "    ,
+            "Daniel "    ,
+            "Carter "    ,
+            "Tyler  "    ,
+            "Cameron"    ,
+            "Christian"  ,
+            "Wyatt  "    ,
+            "Henry  "    ,
+            "Eli    "    ,
+            "Joseph "    ,
+            "Max    "    ,
+            "Isaac  "    ,
+            "Samuel "    ,
+            "Anthony"    ,
+            "Grayson"    ,
+            "Zachary"    ,
+            "David  "    ,
+            "Christopher",
+            "John   "    ,
+            "Isaiah "    ,
+            "Levi   "    ,
+            "Jonathan"   ,
+            "Oliver "    ,
+            "Chase  "    ,
+            "Cooper "    ,
+            "Tristan"    ,
+            "Colton "    ,
+            "Austin "    ,
+            "Colin  "    ,
+            "Charlie"    ,
+            "Dominic"    ,
+            "Parker "    ,
+            "Hunter "    ,
+            "Thomas "    ,
+            "Alex   "    ,
+            "Ian    "    ,
+            "Jordan "    ,
+            "Cole   "    ,
+            "Julian "    ,
+            "Aaron  "    ,
+            "Carson "    ,
+            "Miles  "    ,
+            "Blake  "    ,
+            "Brody  "    ,
+            "Adam   "    ,
+            "Sebastian"  ,
+            "Adrian "    ,
+            "Nolan  "    ,
+            "Sean   "    ,
+            "Riley  "    ,
+            "Bentley"    ,
+            "Xavier "    ,
+            "Hayden "    ,
+            "Jeremiah"   ,
+            "Jason  "    ,
+            "Jake   "    ,
+            "Asher  "    ,
+            "Micah  "    ,
+            "Jace   "    ,
+            "Brandon"    ,
+            "Josiah "    ,
+            "Hudson "    ,
+            "Nathaniel"  ,
+            "Bryson "    ,
+            "Ryder  "    ,
+            "Justin "    ,
+            "Bryce  "    ,
+        };
+
+        #endregion
+    }
+}
